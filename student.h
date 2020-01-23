fd728beae3295a8d9b5fef1593fbf6850f0c0ec5
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-01-23 07:47:09 -0800

    Update student.h

--- student.h
+++ student.h
@@ -6,17 +6,26 @@
 using namespace std;
 
 class student
-{
+{//Student.h
  public:
   student(char*, char*, int, float);
+ //Student Constructor
   char* getFirstName();
+  //Get first name
   char* getLastName();
+  //Get last name
   int getID();
+  //Get ID
   float getGPA();
+  //Get GPA
  private:
   char* firstName;
+  //First name
   char* lastName;
+  //Last name
   int ID;
+  //ID
   float GPA;
+  //GPA
 };
 #endif
